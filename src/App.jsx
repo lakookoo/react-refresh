3bd243f3b924e4b21d3e82e3d8a0dcff5d49be65
Larisa <kosareva.larisa@gmail.com>
2024-04-04 18:55:53 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,53 @@
+import React from "react";
+import reactImg from "./assets/react-core-concepts.png"
+import componentsImg from "./assets/components.png"
+
+
+function Header() {
+	return (<header>
+		<img src={reactImg} alt="Stylized atom" />
+		<h1>React Essentials</h1>
+		<p>
+			Fundamental React concepts you will need for almost any app you are
+			going to build!
+		</p>
+	</header>
+	);
+}
+
+function CoreConcept(props) {
+	return (
+		<li>
+			<img src={props.image} alt="..." />
+			<h3>Title</h3>
+			<p>Description</p>
+		</li>
+	);
+}
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<main>
+				<section id="core-concepts">
+					<h2>Core Concepts</h2>
+					<ul>
+						<CoreConcept
+							title="Components"
+							description="The core UI building block"
+							image={componentsImg}
+						/>
+						<CoreConcept />
+						<CoreConcept />
+						<CoreConcept />
+					</ul>
+				</section>
+
+
+			</main>
+		</div>
+	);
 }
 
 export default App;
