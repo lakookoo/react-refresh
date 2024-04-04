afd8bc644257120113c934a11ef648659f7a595b
Larisa <kosareva.larisa@gmail.com>
2024-04-04 19:10:06 +0200

    spread operator

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function Header() {
 function CoreConcept(props) {
 	return (
 		<li>
-			<img src={props.image} alt="..." />
+			<img src={props.image} alt={props.title} />
 			<h3>Title</h3>
 			<p>Description</p>
 		</li>
@@ -33,26 +33,11 @@ function App() {
 				<section id="core-concepts">
 					<h2>Core Concepts</h2>
 					<ul>
-						<CoreConcept
-							title={CORE_CONCEPTS[0].title}
-							description={CORE_CONCEPTS[0].description}
-							image={CORE_CONCEPTS[0].image}
-						/>
-						<CoreConcept
-							title={CORE_CONCEPTS[1].title}
-							description={CORE_CONCEPTS[1].description}
-							image={CORE_CONCEPTS[1].image}
-						/>
-						<CoreConcept
-							title={CORE_CONCEPTS[2].title}
-							description={CORE_CONCEPTS[2].description}
-							image={CORE_CONCEPTS[2].image}
-						/>
-						<CoreConcept
-							title={CORE_CONCEPTS[3].title}
-							description={CORE_CONCEPTS[3].description}
-							image={CORE_CONCEPTS[3].image}
-						/>
+						<CoreConcept {...CORE_CONCEPTS[0]}/>
+						<CoreConcept {...CORE_CONCEPTS[1]}/>
+						<CoreConcept {...CORE_CONCEPTS[2]}/>
+						<CoreConcept {...CORE_CONCEPTS[3]}/>
+						
 					</ul>
 				</section>
 
