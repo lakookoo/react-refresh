e610a4a955d5c90c15d486ad98d4eed0a8aedf2f
Larisa <kosareva.larisa@gmail.com>
2024-04-04 19:07:45 +0200

    another way of using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import reactImg from "./assets/react-core-concepts.png"
-import componentsImg from "./assets/components.png"
+import { CORE_CONCEPTS } from "./data.js"
+import  reactImg  from "./assets/react-core-concepts.png"
 
 
 function Header() {
@@ -34,13 +34,25 @@ function App() {
 					<h2>Core Concepts</h2>
 					<ul>
 						<CoreConcept
-							title="Components"
-							description="The core UI building block"
-							image={componentsImg}
+							title={CORE_CONCEPTS[0].title}
+							description={CORE_CONCEPTS[0].description}
+							image={CORE_CONCEPTS[0].image}
 						/>
-						<CoreConcept />
-						<CoreConcept />
-						<CoreConcept />
+						<CoreConcept
+							title={CORE_CONCEPTS[1].title}
+							description={CORE_CONCEPTS[1].description}
+							image={CORE_CONCEPTS[1].image}
+						/>
+						<CoreConcept
+							title={CORE_CONCEPTS[2].title}
+							description={CORE_CONCEPTS[2].description}
+							image={CORE_CONCEPTS[2].image}
+						/>
+						<CoreConcept
+							title={CORE_CONCEPTS[3].title}
+							description={CORE_CONCEPTS[3].description}
+							image={CORE_CONCEPTS[3].image}
+						/>
 					</ul>
 				</section>
 
