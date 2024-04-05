01a692e198cb82348d55c1bd9db0734ded99ae40
Larisa <kosareva.larisa@gmail.com>
2024-04-05 19:01:10 +0200

    use of children props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CORE_CONCEPTS } from "./data.js"
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
 	return (
@@ -17,6 +18,12 @@ function App() {
 						<CoreConcept {...CORE_CONCEPTS[3]}/>
 						
 					</ul>
+				</section>
+				<section id="examples">
+					<h2>Examples</h2>
+					<menu>
+						<TabButton>Components</TabButton>
+					</menu>
 				</section>
 
 
